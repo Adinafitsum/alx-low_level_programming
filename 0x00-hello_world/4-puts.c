f11248488b3dff3a6823d9931ad1ab40cdcdb9ac
Adinafitsum <fasresahegne@gmail.com>
2022-10-18 18:51:42 -0700

    first line of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/*
+ * main - print a line of code  using puts
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
+
+
+	
